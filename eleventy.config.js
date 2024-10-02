6eb81116c0337709221e84c41f0af7481bb677dd
Sasha Mansurov <sasha@mansurov.me>
2024-10-02 17:28:28 +0200

    remove dot?

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,19 +3,19 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addPassthroughCopy("./src/static/style.css");
-  eleventyConfig.addPassthroughCopy("./src/static/fonts");
-  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("src/static/style.css");
+  eleventyConfig.addPassthroughCopy("src/static/fonts");
+  eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy(
-    { "./src/static/favicon.svg": "./" },
+    { "src/static/favicon.svg": "./" },
     {
       debug: true, // log debug information
     },
   );
 
-  eleventyConfig.addPassthroughCopy("./src/static/chess.js");
-  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.js");
-  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.css");
+  eleventyConfig.addPassthroughCopy("src/static/chess.js");
+  eleventyConfig.addPassthroughCopy("src/static/lichess-pgn-viewer.js");
+  eleventyConfig.addPassthroughCopy("src/static/lichess-pgn-viewer.css");
 
   return {
     dir: {
