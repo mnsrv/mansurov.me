adabfdfb84b8f1f8a5cd3e450fac76346e03c16a
Sasha Mansurov <sasha@mansurov.me>
2024-10-02 17:15:50 +0200

    Try to fix deploy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,21 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addPassthroughCopy("./src/static/style.css")
-  eleventyConfig.addPassthroughCopy("./src/static/fonts")
+  eleventyConfig.addPassthroughCopy("./src/static/style.css");
+  eleventyConfig.addPassthroughCopy("./src/static/fonts");
   eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy({ "./src/static/favicon.svg": "/" })
+  eleventyConfig.addPassthroughCopy("./src/static/favicon.svg");
 
-  eleventyConfig.addPassthroughCopy("./src/static/chess.js")
-  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.js")
-  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.css")
+  eleventyConfig.addPassthroughCopy("./src/static/chess.js");
+  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.js");
+  eleventyConfig.addPassthroughCopy("./src/static/lichess-pgn-viewer.css");
 
   return {
     dir: {
-      input: 'src',
-      output: 'public'
-    }
-  }
+      input: "src",
+      output: "public",
+    },
+  };
 }
