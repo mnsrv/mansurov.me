85109b9241cce9b78c4a1f8f7a9dbc6f924a32db
Sasha Mansurov <sasha@mansurov.me>
2025-02-10 16:20:07 +0100

    Remove quotes from books everywhere

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -6,9 +6,6 @@ export default {
     return [];
   },
   summaryQuotes: (data) => {
-    if (data.layout === "post.liquid" && data.summary) {
-      return data.quotes.filter((q) => q.date.startsWith(data.summary));
-    }
     if (data.layout === "post.liquid" && data.summaryBook) {
       return data.quotes.filter((b) => b.song === data.summaryBook);
     }
--- src/quotes.md
+++ src/quotes.md
@@ -9,9 +9,11 @@ layout: "base.liquid"
 ---
 
 {% for quote in quotes %}
+{% if quote.type != "book" %}
 
 > {{ quote.title | newline_to_br }}
 >
 > — {{ quote.author }}, _{% if quote.link %}[{{ quote.song }}](/blog/{{quote.link}}){% else %}{{ quote.song }}{% endif %}_
 
+{% endif %}
 {% endfor %}
